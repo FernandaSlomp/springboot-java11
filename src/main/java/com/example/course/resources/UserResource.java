a84bf366ea87453925b58888cde926a01ecaa13b
Fernanda Slomp <fernandaslomp@hotmail.com>
2021-06-01 16:48:37 -0300

    criando user

---
+++ src/main/java/com/example/course/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.example.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.course.entities.User;
+
+@RestController
+@RequestMapping(value ="/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> finAll(){
+		User u = new User(1L ,"teste", "teste@gmail.com", "392748324932", "1234");
+		return ResponseEntity.ok().body(u);
+	}
+	
+
+}
